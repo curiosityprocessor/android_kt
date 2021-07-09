cac115497962bdc84208433d93a6127c6ba26e6f
curiosityprocessor <curiosityprocessor@gmail.com>
2021-07-09 14:03:00 +0900

    update MainActivity.kt, activity_main.xml create ResultActivity.kt, activity_result.xml

---
+++ android/android_kt_toy/BmiCalculator/app/src/main/java/fastcampus/aop/part2/ch1/bmicalculator/ResultActivity.kt
@@ -0,0 +1,17 @@
+package fastcampus.aop.part2.ch1.bmicalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        val height = intent.getIntExtra("height", 0)
+        val weight = intent.getIntExtra("weight", 0)
+
+        Log.d("ResultActivity", "height:$height, weight:$weight")
+    }
+}
