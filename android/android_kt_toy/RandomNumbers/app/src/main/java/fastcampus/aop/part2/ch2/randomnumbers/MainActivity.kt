7683a3699bc652fff4c9def6f3b93385eec1f83f
cyjoo <38976452+cyjoo@users.noreply.github.com>
2021-07-13 17:35:13 +0900

    update activity_main.xml

--- android/android_kt_toy/RandomNumbers/app/src/main/java/fastcampus/aop/part2/ch2/randomnumbers/MainActivity.kt
+++ android/android_kt_toy/RandomNumbers/app/src/main/java/fastcampus/aop/part2/ch2/randomnumbers/MainActivity.kt
@@ -7,5 +7,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val random = java.util.Random()
+        val numberSet = mutableSetOf<Int>()
+        while(numberSet.size < 6) {
+            val randomNo = random.nextInt(45) + 1
+            numberSet.add(randomNo)
+        }
+
+        val numberList = mutableListOf<Int>().apply {
+            for (i in 1..45){this.add(i)}
+        }
+        numberList.shuffle()
+
+        numberList.subList(0, 6)
+
+
     }
 }
