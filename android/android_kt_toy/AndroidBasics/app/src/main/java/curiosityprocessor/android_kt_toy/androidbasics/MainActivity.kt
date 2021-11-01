ef2f0519b1251fd9c68c88c34a2ec50dbfb25d0d
cyjoo1988 <38976452+cyjoo1988@users.noreply.github.com>
2021-11-01 13:47:13 +0900

    init mini-project "AndroidBasics"

---
+++ android/android_kt_toy/AndroidBasics/app/src/main/java/curiosityprocessor/android_kt_toy/androidbasics/MainActivity.kt
@@ -0,0 +1,11 @@
+package curiosityprocessor.android_kt_toy.androidbasics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
